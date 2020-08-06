eec8eaebb82ef7b7dd56268b8021e166739fe762
zaina-mashni <59286120+zaina-mashni@users.noreply.github.com>
2020-08-06 13:57:58 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ELB-746012067.us-east-1.elb.amazonaws.com:80'
+  apiUrl: 'http://ELB-746012067.us-east-1.elb.amazonaws.com:8080'
 };
